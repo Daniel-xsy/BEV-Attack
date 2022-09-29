b32623827388bb27b7ec9ea2f045357624e5256c
Daniel-xsy <1491387884@qq.com>
2022-09-29 01:55:15 +0000

    add bound

--- zoo/BEVFormer/attacks/attacker/pgd.py
+++ zoo/BEVFormer/attacks/attacker/pgd.py
@@ -36,34 +36,31 @@ class PGD(BaseAttacker):
         model.eval()
 
         img_ = img[0].data[0].clone()
+        B, M, C, H, W = img_.size()
 
         if self.category == "trades":
             x_adv = img_.detach() + 0.001 * torch.randn(img_.shape).to(img_.device).detach() if self.rand_init else img_.detach()
 
         if self.category == "Madry":
             x_adv = img_.detach() + torch.from_numpy(np.random.uniform(-self.epsilon, self.epsilon, img_.shape)).float().to(img_.device) if self.rand_init else img_.detach()
-            # x_adv = torch.clamp(x_adv, 0.0, 1.0)
+        x_adv = torch.clamp(x_adv, self.lower.view(1, 1, C, 1, 1), self.upper.view(1, 1, C, 1, 1))
 
         for k in range(self.num_steps):
         
-            x_adv.requires_grad_()
+            # x_adv.requires_grad_()
             img[0].data[0] = x_adv
             inputs = {'img': img, 'img_metas': img_metas}
             # with torch.no_grad():
             outputs = model(return_loss=False, rescale=True, **inputs)
-
-            with torch.enable_grad():
-                # -----------------------------
-                # TODO: add GT in attacker
-                # -----------------------------
-                assign_results = self.assigner.assign(outputs, gt_bboxes_3d, gt_labels_3d)
-                loss_adv = self.loss_fn(**assign_results)
+            # assign pred bbox to ground truth
+            assign_results = self.assigner.assign(outputs, gt_bboxes_3d, gt_labels_3d)
+            loss_adv = self.loss_fn(**assign_results)
 
             loss_adv.backward()
             eta = self.step_size * x_adv.grad.sign()
             x_adv = x_adv.detach() + eta
             x_adv = torch.min(torch.max(x_adv, img_ - self.epsilon), img_ + self.epsilon)
-            # x_adv = torch.clamp(x_adv, 0.0, 1.0)
+            x_adv = torch.clamp(x_adv, self.lower.view(1, 1, C, 1, 1), self.upper(1, 1, C, 1, 1))
 
         return x_adv
 
