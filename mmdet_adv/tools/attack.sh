45c963a841a0e44617d9ba92efd478b9093b808b
Daniel-xsy <1491387884@qq.com>
2022-10-23 02:41:18 +0000

    update

--- mmdet_adv/tools/attack.sh
+++ mmdet_adv/tools/attack.sh
@@ -9,24 +9,24 @@ projects/configs/attack/bevformer_base_adv.py \
 # -m debugpy --listen 5679 --wait-for-client 
 # BEVDepth 
 # config = 'projects/configs/attack/bevdepth-r50.py'
-# checkpoint_path = '/home/cixie/shaoyuan/BEV-Attack/models/bevdepth/bevdepth-r50.pth'
+# checkpoint_path = '../models/bevdepth/bevdepth-r50.pth'
 
 # FCOS3D
 # config = 'projects/configs/attack/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d.py'
-# checkpoint_path = '/home/cihangxie/shaoyuan/BEV-Attack/models/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_finetune_20210717_095645-8d806dc2.pth'
+# checkpoint_path = '../models/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_finetune_20210717_095645-8d806dc2.pth'
 
 # BEVFormer
 # config = 'projects/configs/attack/bevformer_base_adv.py'
-# checkpoint_path = '/home/cihangxie/shaoyuan/BEV-Attack/models/bevformer/bevformer_r101_dcn_24ep.pth'
+# checkpoint_path = '../models/bevformer/bevformer_r101_dcn_24ep.pth'
 
 # BEVFormer tiny
 # config = '/home/cixie/shaoyuan/BEV-Attack/mmdet_adv/projects/configs/attack/bevformer_tiny.py'
-# checkpoint_path = '/home/cixie/shaoyuan/BEV-Attack/models/bevformer/bevformer_tiny_epoch_24.pth'
+# checkpoint_path = '../models/bevformer/bevformer_tiny_epoch_24.pth'
 
 # DETR3D
 # config = 'projects/configs/attack/detr3d_adv.py'
-# checkpoint_path = '/home/cihangxie/shaoyuan/BEV-Attack/models/detr3d/detr3d_resnet101_cbgs.pth'
+# checkpoint_path = '../models/detr3d/detr3d_resnet101_cbgs.pth'
 
 # PGD
 # config = 'projects/configs/attack/pgd_r101_caffe_fpn_gn-head_2x16_1x_nus-mono3d.py'
-# checkpoint_path = '/home/cihangxie/shaoyuan/BEV-Attack/models/pgd/pgd_r101_caffe_fpn_gn-head_2x16_2x_nus-mono3d_finetune_20211114_162135-5ec7c1cd.pth'
+# checkpoint_path = '../models/pgd/pgd_r101_caffe_fpn_gn-head_2x16_2x_nus-mono3d_finetune_20211114_162135-5ec7c1cd.pth'
